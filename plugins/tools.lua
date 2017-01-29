f243f77db93e635f6c91fdf9efb532c1e3c4ebe6
spam2017 <spam_2@yahoo.com>
2017-01-29 03:49:55 +0330

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -570,7 +570,7 @@ end
 if matches[1] == 'sudolist' and is_sudo(msg) then
 return sudolist(msg)
     end
-if matches[1] == 'beyond' then
+if matches[1] == 'delete' then
 return tdcli.sendMessage(msg.chat_id_, msg.id_, 1, _config.info_text, 1, 'html')
     end
 if matches[1] == 'adminlist' and is_admin(msg) then
@@ -613,7 +613,7 @@ patterns = {
 "^[!/#](admindem) (.*)$",
 "^[!/#](leave)$",
 "^[!/#](autoleave) (.*)$", 
-"^[!/#](beyond)$",
+"^[!/#](delete)$",
 "^[!/#](creategroup) (.*)$",
 "^[!/#](createsuper) (.*)$",
 "^[!/#](tosuper)$",
